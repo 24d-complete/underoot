2dddae530aa6052815fb593d9a5d6830d5df9e9b
Akhil Bhai <akhil.the.56@gmail.com>
2026-01-25 10:31:12 +0530

    fix(msi): use script-relative path for icons and fix debug-msi mockup

--- build/windows/msi/build.sh
+++ build/windows/msi/build.sh
@@ -13,13 +13,13 @@ if [[ "${VSCODE_QUALITY}" == "insider" ]]; then
   PRODUCT_NAME="${APP_NAME} - Insiders"
   PRODUCT_CODE="${APP_NAME}Insiders"
   PRODUCT_UPGRADE_CODE="1C9B7195-5A9A-43B3-B4BD-583E20498467"
-  ICON_DIR="$( cd "${CALLER_DIR}/../vscode/resources/win32" && pwd -W )"
+  ICON_DIR="$( cd "../../../vscode/resources/win32" && pwd -W )"
   SETUP_RESOURCES_DIR=".\\resources\\insider"
 else
   PRODUCT_NAME="${APP_NAME:-Underoot}"
   PRODUCT_CODE="${APP_NAME:-Underoot}"
   PRODUCT_UPGRADE_CODE="965370CD-253C-4720-82FC-2E6B02A53808"
-  ICON_DIR="$( cd "${CALLER_DIR}/../vscode/resources/win32" && pwd -W )"
+  ICON_DIR="$( cd "../../../vscode/resources/win32" && pwd -W )"
   SETUP_RESOURCES_DIR=".\\resources\\stable"
 fi
 
